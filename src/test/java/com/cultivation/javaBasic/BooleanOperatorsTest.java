743736a44168011f26d46fe3814627419b25aa60
lpdft <772346730@qq.com>
2020-08-09 16:31:08 +0800

    [ruyue] Android-BooleanType: test

--- src/test/java/com/cultivation/javaBasic/BooleanOperatorsTest.java
+++ src/test/java/com/cultivation/javaBasic/BooleanOperatorsTest.java
@@ -41,7 +41,7 @@ class BooleanOperatorsTest {
 
         // TODO: please write down the result directly to pass the test.
         // <--start
-        final int expected = 0;
+        final int expected = 0x0004_ab00;
         // --end-->
 
         assertEquals(expected, value & mask);
