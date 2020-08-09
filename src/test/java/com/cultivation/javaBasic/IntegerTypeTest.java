a8fb6a0867696761a8fe46397fefa66e560160fe
lpdft <772346730@qq.com>
2020-08-09 16:03:51 +0800

    [ruyue] Android-IntType: should_overflow_silently test

--- src/test/java/com/cultivation/javaBasic/IntegerTypeTest.java
+++ src/test/java/com/cultivation/javaBasic/IntegerTypeTest.java
@@ -75,7 +75,7 @@ class IntegerTypeTest {
 
         // TODO: Please correct the value to pass the test.
         // <--start
-        final int expectedResult = 0;
+        final int expectedResult = 0x80000000;
         // --end-->
 
         assertEquals(expectedResult, theNumberWillOverflow);
